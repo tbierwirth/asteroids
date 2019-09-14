7870fa2213c67f3c5a2fd01cced8566c5840c3e8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-09-14 15:40:44 -0600

    Add facade to index action and structure date params

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,4 +1,9 @@
 class MostDangerousDayController < ApplicationController
   def index
+    @start_date = (params[:start_date]).to_date.strftime("%B %d, %Y")
+    @end_date = (params[:end_date]).to_date.strftime("%B %d, %Y")
+    render locals: {
+      facade: AsteroidSearch.new(params[:start_date].to_date.strftime, params[:end_date].to_date.strftime)
+    }
   end
 end
